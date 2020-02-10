60dbd3b2a376cf98aa681cf110f34a32cad0d49b
danmaq <info@danmaq.com>
2020-02-10 19:44:00 +0900

    feat: added a atoms/Article component

---
+++ src/components/atoms/Article.tsx
@@ -0,0 +1,31 @@
+import { makeStyles, Typography, TypographyTypeMap } from '@material-ui/core';
+import { DefaultComponentProps } from '@material-ui/core/OverridableComponent';
+import classNames from 'classnames';
+import React from 'react';
+
+export interface Props
+  extends Omit<DefaultComponentProps<TypographyTypeMap>, 'className'> {
+  className?: Parameters<typeof classNames>[number];
+}
+
+const useStyles = makeStyles({
+  root: {
+    letterSpacing: '0.05rem',
+    lineHeight: '190%',
+    paddingBottom: '1rem',
+    textAlign: 'left'
+  }
+});
+
+const Article: React.FC<Props> = ({ children, className, ...rest }) => {
+  const { root } = useStyles();
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Typography className={classNames(root, className)} {...rest}>
+      {children}
+    </Typography>
+  );
+};
+Article.displayName = 'Article';
+
+export default Article;
