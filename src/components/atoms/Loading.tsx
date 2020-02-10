3f155101d5cb14f71acf70a431d00372196cfe4a
danmaq <info@danmaq.com>
2020-02-10 19:45:06 +0900

    feat: added a atoms/Loading component

---
+++ src/components/atoms/Loading.tsx
@@ -0,0 +1,7 @@
+import { LinearProgress } from '@material-ui/core';
+import React from 'react';
+
+export const DOM: React.FC = () => <LinearProgress />;
+DOM.displayName = 'Loading';
+
+export default DOM;
