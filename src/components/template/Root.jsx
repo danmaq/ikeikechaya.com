00a64ea8b72e8535ac13b2c5d3d60c9456cbc8f7
danmaq <info@danmaq.com>
2020-02-11 20:31:33 +0900

    feat: improved a root template component

--- src/components/template/Root.jsx
+++ src/components/template/Root.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-/** @typedef {import('react-static').DocumentComponent} DocumentComponent */
+/** @typedef {import('react-static').DocumentProps<{}>} DocumentProps */
+/** @typedef {import('react-static').RouteFlags} RouteFlags */
+/** @typedef {Omit<DocumentProps, 'state'> & { state: Pick<DocumentProps['state'], 'siteData'> & RouteFlags }} Props */
 
-/** @type {DocumentComponent} */
-const Root = ({ Body, children, Head }) => (
+/** @type {React.FC<Props>} */
+const Root = ({ Body, children, Head, state }) => (
   <html lang="ja">
     <Head>
       <meta charSet="UTF-8" />
@@ -21,6 +23,35 @@ const Root = ({ Body, children, Head }) => (
         name="verify-v1"
       />
       <meta content="9f28bd124221bcb5" name="y-key" />
+      {(state.stage === 'prod' && !state.staging) || (
+        <meta name="robots" content="noindex,nofollow,noarchive" />
+      )}
+      <meta name="author" content="Shuhei Nomura (@danmaq)" />
+      <meta
+        name="description"
+        content="萩池々茶屋は、萩越ヶ浜の漁師 第五良栄丸が経営するレストランです。 魚介類を産地から都心部に直送する前に、漁船より直接水揚げするため、鮮度の良い海産物が召し上がれます。 萩池々茶屋は、明神池が眺望できるお店です。萩の地域活性化に貢献することを企業理念としております。"
+      />
+      <meta
+        name="keywords"
+        content={[
+          '萩池々茶屋',
+          '池々茶屋',
+          '幻の魚',
+          '幻の貝',
+          '萩の梅貝',
+          '萩いけいけ',
+          '萩のバイ貝',
+          '萩',
+          'ランチ',
+          'ディナー',
+          '食事',
+          '梅貝',
+          '明神池',
+          '越ケ浜',
+          '笠山'
+        ].join(',')}
+      />
+      <title>萩池々茶屋</title>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap"
