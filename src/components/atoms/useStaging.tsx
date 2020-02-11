9c25848e24911570cceb4a1f6f8733b16f7d851b
danmaq <info@danmaq.com>
2020-02-11 20:34:05 +0900

    feat: removed the dynamic head component

--- src/components/atoms/useStaging.tsx
+++
@@ -1,9 +0,0 @@
-import { useSiteData, RouteFlags } from 'react-static';
-
-const useStaging = () => {
-  const { routeFlags } = useSiteData<Record<'routeFlags', RouteFlags>>();
-
-  return routeFlags.staging || routeFlags.stage === 'dev';
-};
-
-export default useStaging;
