68cfb630dcc13bd997b9e226164baf6cf891efb1
danmaq <info@danmaq.com>
2020-02-11 22:54:52 +0900

    feat: improved the page padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import { Router } from '@reach/router';
 import React from 'react';
@@ -9,6 +10,10 @@ import Loading from './components/atoms/Loading';
 import Footer from './components/organisms/Footer';
 import Header from './components/organisms/Header';
 
+const useStyles = makeStyles({
+  innerContainer: { paddingTop: '2rem' }
+});
+
 const App: React.FC = () => (
   <Root>
     <ThemeProvider theme={theme}>
@@ -17,9 +22,11 @@ const App: React.FC = () => (
         <Header />
         <Container component="main" maxWidth="md">
           <React.Suspense fallback={<Loading />}>
-            <Router>
-              <Routes path="*" />
-            </Router>
+            <div className={useStyles().innerContainer}>
+              <Router>
+                <Routes path="*" />
+              </Router>
+            </div>
           </React.Suspense>
         </Container>
         <Footer />
