8e3c04b21161bd9d349dd890f2b9dfa4e861ba47
danmaq <info@danmaq.com>
2020-02-13 22:31:55 +0900

    fix: improved the seo

--- src/components/template/Root.jsx
+++ src/components/template/Root.jsx
@@ -5,18 +5,18 @@ import React from 'react';
 /** @typedef {import('react-static').RouteFlags} RouteFlags */
 /** @typedef {import('react-static').Route} Route */
 
-/** @typedef {Pick<DocumentProps, 'Body' | 'Head'>} DOMPropsBase */
+/** @typedef {Pick<DocumentProps, 'Body' | 'Head' | 'Html'>} DOMPropsBase */
 
 /**
  * @typedef DOMPropsExtends
- * @property {string} [author]
  * @property {string} [baseUrl]
- * @property {string} [classification]
- * @property {Date} [date]
  * @property {string} [description]
  * @property {string} [descriptionShort]
- * @property {string} [keywords]
+ * @property {string} [icon]
  * @property {string} lang
+ * @property {number} [latitude]
+ * @property {string} [locale]
+ * @property {number} [longitude]
  * @property {boolean} [norobot]
  * @property {string} [path]
  * @property {string} title
@@ -26,135 +26,158 @@ import React from 'react';
 
 /** @type {React.FC<DOMProps>} */
 const DOM = ({
-  author,
   baseUrl,
   Body,
-  classification,
-  date,
   children,
   description,
   descriptionShort,
   Head,
-  keywords,
+  Html,
+  icon,
   lang,
+  locale,
+  latitude,
+  longitude,
   norobot,
   path,
   title
-}) => (
-  <html lang={lang} xmlns="http://www.w3.org/1999/xhtml">
-    <Head>
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="x-ua-compatible" content="ie=Edge" />
-      <meta name="author" content={author} />
-      {date && <meta name="keywords" content={date.toISOString()} />}
-      <meta name="classification" content={classification} />
-      <meta name="code" scheme="081" />
-      <meta name="color-scheme" content="only light" />
-      {(description || descriptionShort) && (
-        <meta name="description" content={description || descriptionShort} />
-      )}
-      <meta
-        name="google-site-verification"
-        content="EYCxByEbK1022IJuw0n02MKiiSNDOjLPHoAdmpO0mKM"
-      />
-      {keywords && <meta name="keywords" content={keywords} />}
-      <meta name="referrer" content="origin-when-crossorigin" />
-      {norobot && (
+}) => {
+  const color = '#0C0C88';
+  const desc = !!(description || descriptionShort);
+  const pos = !!(latitude && longitude);
+  const url = `${baseUrl}/${path}`;
+  const image = `${baseUrl}/images/home/slideshow00.jpg`;
+  return (
+    <Html
+      lang={lang}
+      itemscope
+      itemtype="https://schema.org/Article"
+      xmlns="http://www.w3.org/1999/xhtml"
+    >
+      <Head prefix="og: http://ogp.me/ns# website: http://ogp.me/ns/website#">
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="cleartype" content="on" />
         <meta
-          name="robots"
-          content="noindex,nofollow,noodp,noarchive,nosnippet,noimageindex"
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'"
         />
-      )}
-      <meta name="tel" scheme="0838217111" />
-      <meta name="theme-color" content="#0C0C88" />
-      <meta
-        name="verify-v1"
-        content="uFDAqcIHu44EC4Fnw5dfDQRx3UrbY+FtONeiafAkXeo="
-      />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=5, shrink-to-fit=no, viewport-fit=cover"
-      />
-      <meta name="y-key" content="9f28bd124221bcb5" />
-      {(descriptionShort || description) && (
+        <meta httpEquiv="x-ua-compatible" content="ie=Edge" />
+        <title>{title}</title>
+        {desc && (
+          <meta
+            itemProp="description"
+            content={description || descriptionShort}
+          />
+        )}
+        <meta itemProp="image" content={image} />
+        <meta itemProp="name" content={title} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content={color} />
+        <meta name="apple-mobile-web-app-title" content={title} />
+        <meta name="application-name" content={title} />
+        <meta name="color-scheme" content="only light" />
+        {desc && (
+          <meta name="description" content={description || descriptionShort} />
+        )}
+        <meta name="geo.placename" content={title} />
+        {pos && (
+          <meta name="geo.position" content={`${latitude};${longitude}`} />
+        )}
+        <meta name="geo.region" content="JP" />
         <meta
-          property="og:description"
-          content={descriptionShort || description}
+          name="google-site-verification"
+          content="EYCxByEbK1022IJuw0n02MKiiSNDOjLPHoAdmpO0mKM"
         />
-      )}
-      <meta
-        property="og:image"
-        content={`${baseUrl}/images/home/slideshow00.jpg`}
-      />
-      <meta property="og:locale" content={lang} />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
-      <meta name="og:url" content={`${baseUrl}/${path}`} />
-      <meta name="twitter:card" content="summary_large_image" />
-      {author && <meta name="twitter:creator" content={author} />}
-      <title>{title}</title>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap"
-      />
-      <link href="/" rel="bookmark" />
-    </Head>
-    <Body>
-      {children}
-      <script
-        crossOrigin="anonymous"
-        src="https://polyfill.io/v3/polyfill.min.js"
-      />
-    </Body>
-  </html>
-);
+        {pos && <meta name="ICBM" content={`${latitude}, ${longitude}`} />}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-starturl" content="/" />
+        <meta name="referrer" content="origin-when-crossorigin" />
+        <meta name="renderer" content="webkit" />
+        {norobot && (
+          <meta
+            name="robots"
+            content="noindex,nofollow,noodp,noarchive,nosnippet,noimageindex"
+          />
+        )}
+        <meta name="theme-color" content={color} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@danmaq" />
+        <meta
+          name="verify-v1"
+          content="uFDAqcIHu44EC4Fnw5dfDQRx3UrbY+FtONeiafAkXeo="
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5, shrink-to-fit=no, viewport-fit=cover"
+        />
+        <meta name="y-key" content="9f28bd124221bcb5" />
+        {desc && (
+          <meta
+            property="og:description"
+            content={descriptionShort || description}
+          />
+        )}
+        <meta property="og:image" content={image} />
+        <meta
+          property="og:image:alt"
+          content="丸い地球からの恵み: 船上から望む水平線からは、地球そのものを感じることができます。私たちは地球からいただく恩恵を大切にし、多くの方に最良の形でお届けしたいと考えています。 -- produced by 第五良栄丸"
+        />
+        {locale && <meta property="og:locale" content={locale} />}
+        <meta property="og:site_name" content={title} />
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="op:markup_version" content="v1.0" />
+        <link rel="apple-touch-icon" sizes="128x128" href={icon} />
+        <link rel="author" href="/humans.txt" />
+        <link rel="icon" sizes="128x128" href={icon} />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap"
+        />
+        <link rel="bookmark" href="/" />
+        <link rel="canonical" href={url} />
+        <link rel="first" href="/" />
+      </Head>
+      <Body>
+        {children}
+        <script
+          crossOrigin="anonymous"
+          src="https://polyfill.io/v3/polyfill.min.js"
+        />
+      </Body>
+    </Html>
+  );
+};
 DOM.defaultProps = {
   author: '',
   baseUrl: '',
   description: '',
   descriptionShort: '',
-  keywords: '',
   path: ''
 };
 DOM.displayName = 'RootDOM';
-
-/** @type {(...items: string[]) => string} */
-const join = (...items) => items.join(',');
 
 /** @typedef {Pick<DocumentProps['state'], 'siteData'> & RouteFlags & { config: ReactStaticConfig, route: Route }} State */
 
 /** @typedef {Omit<DocumentProps, 'state'> & { state: State }} Props */
 
 /** @type {React.FC<Props>} */
-const Root = ({ Body, children, Head, state }) => (
+const Root = ({ Body, children, Head, Html, state }) => (
   <DOM
-    author="@danmaq"
     baseUrl={state.config.siteRoot}
     Body={Body}
-    classification={join('restaurant', 'fishery')}
-    date={new Date()}
     description="萩池々茶屋は、萩越ヶ浜の漁師 第五良栄丸が経営するレストランです。魚介類を産地から都心部に直送する前に、漁船より直接水揚げするため、鮮度の良い海産物が召し上がれます。萩池々茶屋は、明神池が眺望できるお店です。萩の地域活性化に貢献することを企業理念としております。"
-    descriptionShort="萩池々茶屋は、萩越ヶ浜の漁師 第五良栄丸が経営するレストランです。明神池のほとりから、萩の地域活性化を応援しております。"
+    descriptionShort="萩・越ヶ浜の漁師、第五良栄丸が経営するレストラン。明神池のほとりから、萩の地域活性化を応援しております。"
     Head={Head}
-    keywords={join(
-      '萩池々茶屋',
-      '池々茶屋',
-      '幻の魚',
-      '幻の貝',
-      '萩の梅貝',
-      '萩いけいけ',
-      '萩のバイ貝',
-      '萩',
-      'ランチ',
-      'ディナー',
-      '食事',
-      '梅貝',
-      '明神池',
-      '越ケ浜',
-      '笠山'
-    )}
-    lang="ja_JP"
+    Html={Html}
+    icon="/images/header/conch_128.png"
+    lang="ja"
+    latitude={34.448272}
+    locale="ja_JP"
+    longitude={131.409517}
     norobot={state.stage === 'prod' && !state.staging}
     path={state.route.path}
     title="萩池々茶屋"
