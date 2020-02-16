ca1e395a1ff2122323a7ea6434c0580c06e436f7
danmaq <info@danmaq.com>
2020-02-16 20:46:32 +0900

    feat: refactored the organisms components

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,54 +1,52 @@
-import React from 'react';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 import { makeStyles } from '@material-ui/core/styles';
-import Conch from '~/components/atoms/Conch';
-import Copyright from '~/components/atoms/Copyright';
+import classNames from 'classnames';
+import React from 'react';
+import FooterItem from '~/components/molecules/FooterItem';
 import GlobalNavigation from '~/components/molecules/GlobalNavigation';
 import { footerData } from '~/utils/globalNavigationSource';
 
+export interface DOMProps {
+  className?: string;
+  lineClassName?: string;
+}
+
+export interface Props {
+  className?: Parameters<typeof classNames>[number];
+}
+
+export const DOM: React.FC<DOMProps> = ({
+  children,
+  className,
+  lineClassName
+}) => (
+  <Container className={className} component="footer" maxWidth="md">
+    <hr className={lineClassName} />
+    <Hidden only="xs">
+      <FooterItem conchSize={1} mainSize={11}>
+        {children}
+      </FooterItem>
+    </Hidden>
+    <Hidden smUp>
+      {children}
+      <FooterItem conchSize={2} mainSize={10} />
+    </Hidden>
+  </Container>
+);
+DOM.displayName = 'FooterDOM';
+
 const useStyles = makeStyles({
   divider: { border: '1px solid rgb(217, 217, 217)', margin: 0 },
-  innerContainer: { paddingTop: '1rem' },
   root: { paddingBottom: 'env(safe-area-inset-bottom)' }
 });
 
-export const Footer: React.FC = () => {
-  const { divider, innerContainer, root } = useStyles();
-  const conch = (
-    <Conch
-      action="https://www.google.co.jp/search?tbm=isch&amp;q=%E6%A2%85%E8%B2%9D"
-      dark
-    />
-  );
-  const nav = <GlobalNavigation footer source={footerData} />;
+export const Footer: React.FC<Props> = ({ className }) => {
+  const { divider, root } = useStyles();
   return (
-    <Container className={root} component="footer" maxWidth="md">
-      <hr className={divider} />
-      <Hidden only="xs">
-        <Grid className={innerContainer} container>
-          <Grid item xs={11}>
-            {nav}
-            <Copyright />
-          </Grid>
-          <Grid item xs={1}>
-            {conch}
-          </Grid>
-        </Grid>
-      </Hidden>
-      <Hidden smUp>
-        {nav}
-        <Grid className={innerContainer} container>
-          <Grid item xs={10}>
-            <Copyright />
-          </Grid>
-          <Grid item xs={2}>
-            {conch}
-          </Grid>
-        </Grid>
-      </Hidden>
-    </Container>
+    <DOM className={classNames(root, className)} lineClassName={divider}>
+      <GlobalNavigation footer source={footerData} />
+    </DOM>
   );
 };
 Footer.displayName = 'Footer';
