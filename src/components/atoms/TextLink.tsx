53c0e73e9a3805fb305a560562ffd5cacf3c1983
danmaq <info@danmaq.com>
2020-02-16 20:45:22 +0900

    feat: refactored the atoms components

--- src/components/atoms/TextLink.tsx
+++ src/components/atoms/TextLink.tsx
@@ -1,51 +1,42 @@
 import Link, { LinkTypeMap } from '@material-ui/core/Link';
 import { DefaultComponentProps } from '@material-ui/core/OverridableComponent';
 import { Link as RouterLink } from '@reach/router';
-import classNames from 'classnames';
 import React from 'react';
-import getLinkAttributes from '~/utils/getLinkAttributes';
+import useLinkProps, {
+  AnchorReturnType,
+  RouterReturnType
+} from '~/utils/useLinkProps';
 
-export interface DOMProps
-  extends Pick<
-    DefaultComponentProps<LinkTypeMap>,
-    'className' | 'color' | 'href' | 'rel' | 'target'
-  > {
-  absolute?: boolean;
-}
+export type DOMProps = Pick<
+  DefaultComponentProps<LinkTypeMap>,
+  'className' | 'color'
+> &
+  (AnchorReturnType | RouterReturnType);
 
-export interface Props extends Pick<DOMProps, 'color' | 'href'> {
-  className?: Parameters<typeof classNames>[number];
+export interface Props extends Pick<DOMProps, 'className' | 'color'> {
+  href?: string;
 }
 
 export const DOM: React.FC<DOMProps> = ({
-  absolute,
   children,
   className,
   color,
-  href,
   rel,
-  target
-}) =>
-  absolute ? (
-    <Link
-      className={className}
-      color={color}
-      href={href}
-      rel={rel}
-      target={target}
-    >
-      {children}
-    </Link>
-  ) : (
-    <Link
-      className={className}
-      component={RouterLink}
-      color={color}
-      to={href ?? '/'}
-    >
-      {children}
-    </Link>
-  );
+  target,
+  ...rest
+}) => (
+  <Link
+    className={className}
+    color={color}
+    component={rest.allowRoute ? RouterLink : 'a'}
+    href={rest.allowRoute ? undefined : rest.href}
+    rel={rel}
+    target={target}
+    to={rest.allowRoute ? rest.to : undefined}
+  >
+    {children}
+  </Link>
+);
 
 const TextLink: React.FC<Props> = ({
   children,
@@ -53,15 +44,15 @@ const TextLink: React.FC<Props> = ({
   color = 'error',
   href
 }) => {
-  const { absolute, rel, target } = getLinkAttributes(href ?? '');
+  const { rel, target, ...rest } = useLinkProps(href);
   return (
     <DOM
-      absolute={absolute}
-      className={classNames(className)}
+      className={className}
       color={color}
-      href={href}
       rel={rel}
       target={target}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}
     >
       {children}
     </DOM>
