d6bd9d5af2d13b102aab92a7ce039fd3362715ee
danmaq <info@danmaq.com>
2020-02-16 20:46:50 +0900

    feat: refactored a root component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { Router } from '@reach/router';
+import classNames from 'classnames';
 import React from 'react';
 import { Root, Routes } from 'react-static';
 import Loading from './components/atoms/Loading';
@@ -9,36 +10,54 @@ import Footer from './components/organisms/Footer';
 import Header from './components/organisms/Header';
 import theme from './theme';
 
-const useStyles = makeStyles({
-  innerContainer: {
-    paddingLeft: 'env(safe-area-inset-left)',
-    paddingRight: 'env(safe-area-inset-right)',
-    paddingTop: '2rem'
-  }
-});
+export interface DOMProps {
+  containerClassName?: string;
+  footerClassName?: string;
+  headerClassName?: string;
+}
 
-const App: React.FC = () => (
+const DOM: React.FC<DOMProps> = ({
+  containerClassName,
+  footerClassName,
+  headerClassName
+}) => (
   <Root>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="content">
         <React.Suspense fallback={<Loading />}>
-          <Header />
-          <Container
-            className={useStyles().innerContainer}
-            maxWidth="md"
-            role="main"
-          >
+          <Header containerClassName={headerClassName} />
+          <Container className={containerClassName} maxWidth="md" role="main">
             <Router>
               <Routes path="*" />
             </Router>
           </Container>
-          <Footer />
+          <Footer className={footerClassName} />
         </React.Suspense>
       </div>
     </ThemeProvider>
   </Root>
 );
+DOM.displayName = 'AppDOM';
+
+const useStyles = makeStyles({
+  innerContainer: { paddingTop: '2rem' },
+  safeArea: {
+    paddingLeft: 'env(safe-area-inset-left)',
+    paddingRight: 'env(safe-area-inset-right)'
+  }
+});
+
+const App: React.FC = () => {
+  const { innerContainer, safeArea } = useStyles();
+  return (
+    <DOM
+      containerClassName={classNames(innerContainer, safeArea)}
+      footerClassName={safeArea}
+      headerClassName={safeArea}
+    />
+  );
+};
 App.displayName = 'App';
 
 export default App;
